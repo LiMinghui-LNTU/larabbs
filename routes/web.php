5796d16c2502271137b5b9c9c955de488bc436c4
lmh <2297181256@qq.com>
2019-07-13 17:07:50 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,6 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+//用户相关路由
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
