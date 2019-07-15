821e533c0c39d96e3ba4416a8a0a09d4ac9fda66
lmh <2297181256@qq.com>
2019-07-15 09:50:12 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 //用户相关路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
