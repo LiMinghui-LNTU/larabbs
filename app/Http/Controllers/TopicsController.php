2a9555209a83ee7213a39e7c941c5436fcb183e8
vagrant <vagrant@homestead>
2019-07-16 07:42:43 +0000

    用户可以上传话题图片

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -8,6 +8,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Handlers\ImageUploadHandler;
 
 class TopicsController extends Controller
 {
@@ -62,4 +63,27 @@ class TopicsController extends Controller
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据，默认上传失败
+        $data = [
+            'success'   => false,
+            'msg'       => '上传失败',
+            'file_path' => ''
+        ];
+
+        // 检查是否有图片上传，并赋值给file
+        if($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($file, 'topics', Auth::id(), 1024);
+            // 图片保存成功的话
+            if($result) {
+                $data['success'] = true;
+                $data['msg'] = '上传成功！';
+                $data['file_path'] = $result['path'];
+            }
+        }
+        return $data;
+    }
 }
