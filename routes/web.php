504fca77919b9aeff31e2550d6aa0fee406a66fb
vagrant <vagrant@homestead>
2019-07-18 04:47:06 +0000

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,9 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 //用户相关路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
