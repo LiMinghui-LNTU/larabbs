8a90323b6fe7a5994acbb55970c2ca5ca0170139
vagrant <vagrant@homestead>
2019-07-28 02:15:42 +0000

    用户可以发表评论

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -6,36 +6,16 @@ class ReplyRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            };
-        }
+        return [
+            'content' => 'required|min:2',
+        ];
     }
 
     public function messages()
     {
         return [
-            // Validation messages
+            'content.required' => '评论内容不能为空。',
+            'content.min' => '评论内容至少2个字符。'
         ];
     }
 }
