854b8e66e6d696ffdf63f2ceb9ea9e3d91130c97
vagrant <vagrant@homestead>
2019-07-28 08:04:22 +0000

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,13 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->laravelNotify($instance);
     }
 
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
+
     /**
      * The attributes that are mass assignable.
      *
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -25,11 +25,16 @@
           <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
           <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
         @else
-        <li class="nav-item">
-            <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
-              <i class="fa fa-plus"></i>
+          <li class="nav-item">
+              <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
+                <i class="fa fa-plus"></i>
+              </a>
+          </li>
+          <li class="nav-item notification-badge">
+            <a class="nav-link mr-3 badge badge-pill badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'secondary' }} text-white" href="{{ route('notifications.index') }}">
+              {{ Auth::user()->notification_count }}
             </a>
-        </li>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
