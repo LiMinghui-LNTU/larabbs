7873209dbc98c5fb7e1ede1ab687211dfa7c6ff1
vagrant <vagrant@homestead>
2019-07-29 06:20:32 +0000

    站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,11 @@
     @include('layouts._footer')
   </div>
 
+  <!-- 用户切换工具：sudo-su -->
+  @if (app()->isLocal())
+    @include('sudosu::user-selector')
+  @endif
+
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
 
